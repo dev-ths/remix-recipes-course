07fb45d9307223800d227bf9a33e80216f126d43
dev-ths <alphavo.ths@gmail.com>
2024-05-21 14:29:48 -0700

    updating the settings route

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -1,44 +1,48 @@
-import { NavLink as RemixNavLink, Outlet } from "@remix-run/react";
-import { classNames } from "~/utils/misc";
+import { NavLink as RemixNavLink, Outlet } from "@remix-run/react"
+import { classNames } from "~/utils/misc"
 
 type PageLayoutProps = {
-  title: string;
-  links: Array<{ to: string; label: string }>;
-};
+	title: string
+	links: Array<{ to: string; label: string }>
+}
 export function PageLayout({ title, links }: PageLayoutProps) {
-  return (
-    <div className="flex flex-col h-full">
-      <h1 className="text-2xl font-bold my-4">{title}</h1>
-      <nav className="border-b-2 pb-2 mt-2">
-        {links.map(({ to, label }) => (
-          <NavLink key={label} to={to}>
-            {label}
-          </NavLink>
-        ))}
-      </nav>
-      <div className="py-4 overflow-y-auto">
-        <Outlet />
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex h-full flex-col">
+			<h1 className="my-4 text-2xl font-bold">{title}</h1>
+			<nav className="mt-2 border-b-2 pb-2">
+				{links.map(({ to, label }) => (
+					<NavLink
+						key={label}
+						to={to}
+					>
+						{label}
+					</NavLink>
+				))}
+			</nav>
+			<div className="overflow-y-auto py-4">
+				<Outlet />
+			</div>
+		</div>
+	)
 }
 
 type NavLinkProps = {
-  to: string;
-  children: React.ReactNode;
-};
+	to: string
+	children: React.ReactNode
+}
+
 function NavLink({ to, children }: NavLinkProps) {
-  return (
-    <RemixNavLink
-      to={to}
-      className={({ isActive }) =>
-        classNames(
-          "hover:text-gray-500 pb-2.5 px-2 md:px-4",
-          isActive ? "border-b-2 border-b-primary" : ""
-        )
-      }
-    >
-      {children}
-    </RemixNavLink>
-  );
+	return (
+		<RemixNavLink
+			to={to}
+			className={({ isActive }) =>
+				classNames(
+					"px-2 pb-2.5 hover:text-gray-500 md:px-4",
+					isActive ? "border-b-2 border-b-primary" : ""
+				)
+			}
+		>
+			{children}
+		</RemixNavLink>
+	)
 }
--- app/routes/settings.tsx
+++ app/routes/settings.tsx
@@ -1,5 +1,38 @@
-import { PageLayout } from "~/components/layout";
+import { PageLayout } from "~/components/layout"
 
 export default function Settings() {
-  return <PageLayout title="Settings" links={[{ to: "app", label: "App" }]} />;
+	return (
+		<PageLayout
+			title="Settings"
+			links={[{ to: "app", label: "App" }]}
+		/>
+	)
 }
+
+/* ZTM Settings Page From start of course
+import {ErrorBoundaryComponent,json,LoaderFunction} from "@remix-run/node"
+import {Link,Outlet, useLoaderData} from "remix-run/react"
+import {ErrorBoundary} from "./app/recipes/$recipeId"
+
+export const loader: LoaderFunction = () => {
+  return json({message:"hello, there"})
+}
+
+export default function Settings(){
+  const data = useLoaderData()
+  return <div>
+    <h1>Settings</h1>
+    <p>This is the settings page</p>
+    <p>Message from the loader: {data.message}</p>
+    <nav>
+      <Link to "app">App</Link>
+      <Link to "profile">Profile</Link>
+    </nav>
+    <Outlet/>
+  </div>
+}
+
+export const ErrorBoundary: ErrorBoundaryComponent = () => {
+  return <div>Hey, an unexpected error occurred</div>
+}
+ */
